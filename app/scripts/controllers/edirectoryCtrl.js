a0167c7f39f1fbcc6dbf018e84c80fe1ca709a9e
asingh.ext <ashish9342@gmail.com>
2017-03-06 15:11:42 +0530

    edirectory Pafe updated

--- app/scripts/controllers/edirectoryCtrl.js
+++ app/scripts/controllers/edirectoryCtrl.js
@@ -8,6 +8,36 @@
  * Controller of the yourSpaceApp
  */
 angular.module('yourSpaceApp')
-  .controller("edirectoryCtrl",["$scope", "edirectoryService", function($scope, edirectoryService){
-      $scope.employeeList = employeeList;
+  .controller("edirectoryController",["$scope", "employeeList", function($scope, employeeList){
+    //console.log(employeeList);
+    $scope.employeeList = employeeList.data;
+    $scope.heading = "Employee List";
+    $scope.faClass ="fa fa-sort-desc";
+    $scope.page = 1;
+    $scope.contactToShow = 10;
+    console.log(employeeList);
+
+
+    $scope.pages = Math.ceil($scope.employeeList.length/ $scope.contactToShow);
+    $scope.myQuery = 0;
+    console.log("Total Employee -> " + $scope.employeeList.length);
+    console.log("Contact To Show -> " + $scope.contactToShow);
+    console.log("Total Pages -> " + $scope.pages);
+
+
+    $scope.orderByMe = function(employee) {
+      if($scope.myQuery === 0){
+        $scope.myOrderBy = employee;
+        $scope.myQuery = 1;
+      }
+      else {
+        $scope.query= {};
+        $scope.myQuery = 0;
+      }
+    }
+
+    $scope.nextPage = function(){
+      $scope.page++;
+      console.log("Page -> " + $scope.page);
+    }
   }]);
--- app/scripts/services/edirectory.service.js
+++ app/scripts/services/edirectory.service.js
@@ -1,17 +1,14 @@
 'use strict';
 
-angular
-    .module('App.services')
+angular.module('yourSpaceApp')
     .factory('edirectoryService', function($http) {
-        var fullData = false;
         var success = function(response) {
-            return fullData ? response.data : response.data[0];
+            return response;
         };
         var error = function(response) {
             console.dir(response);
         };
         var getAllEmployees = function() {
-            fullData = true;
             return $http.get('scripts/data/employee.json').then(success, error);
         };
 
